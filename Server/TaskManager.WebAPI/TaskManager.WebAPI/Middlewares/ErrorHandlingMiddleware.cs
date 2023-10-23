ef8a3db0a5556cccca39c1fa08c9f84dafd12ca6
ADRN_NKT <combinerk@gmail.com>
2023-10-23 19:55:41 +0300

    Add web api basics

---
+++ Server/TaskManager.WebAPI/TaskManager.WebAPI/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.Json;
+using TaskManager.WebAPI.Models;
+
+namespace TaskManager.WebAPI.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(
+            RequestDelegate next,
+            ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (ValidationException ex)
+            {
+                await HandleExceptionAsync(context, ex, _logger);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex, _logger);
+            }
+
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            string? result = null;
+            switch (exception)
+            {
+                case RestException re:
+                    context.Response.StatusCode = (int)re.Code;
+                    result = JsonSerializer.Serialize(new
+                    {
+                        errors = re.Errors
+                    });
+                    break;
+                case Exception e:
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    logger.LogError(e, "Unhandled Exception");
+                    result = JsonSerializer.Serialize(new
+                    {
+                        errors = "Server error"
+                    });
+                    break;
+            }
+
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(result ?? "{}");
+        }
+    }
+}
---
+++ Server/TaskManager.WebAPI/TaskManager.WebAPI/Startup.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using TaskManager.DataAccess;
+using TaskManager.DataAccess.Entities.Mapping;
+using TaskManager.WebAPI.Middlewares;
+
+namespace TaskManager.WebAPI
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ProjectsContext>(options =>
+            {
+                options.UseSqlServer(_configuration.GetConnectionString("Projects"));
+            });
+
+            services.AddAutoMapper(c =>
+            {
+                c.AddProfile<ProjectMappingConfiguration>();
+                c.AddProfile<TaskMappingConfiguration>();
+                c.AddProfile<CommentMappingConfiguration>();
+            });
+
+            services.AddMediatR(c =>
+            {
+                c.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+            services.AddMvcCore()
+                .AddApiExplorer();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SupportNonNullableReferenceTypes();
+
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Task manager API", Version = "v1" });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            using (var scope =
+                        app.ApplicationServices.CreateScope())
+            using (var context = scope.ServiceProvider.GetService<ProjectsContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI();
+
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
