cff134b7bae83b47c6fc61125cd080bf1cc92469
ADRN_NKT <combinerk@gmail.com>
2023-10-23 18:56:55 +0300

    Add repositories, delete parts from last task in base class

---
+++ Server/TaskManager.WebAPI/TaskManager.DataAccess/Repositories/ProjectsRepository.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.Drawing;
+
+namespace TaskManager.DataAccess.Repositories
+{
+    public class ProjectsRepository : RepositoryBase<CoreProject, ProjectsContext>
+    {
+        public ProjectsRepository(ProjectsContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+
+        public override async Task<Guid> Create(CoreProject entity)
+        {
+            var dbEntity = MapToEntity<EntityProject>(entity);
+
+            await _context.Projects.AddAsync(dbEntity);
+
+            await Save(dbEntity, EntityState.Added);
+
+            return dbEntity.Id;
+        }
+
+        public override async Task<bool> Delete(Guid id)
+        {
+            var dbEntity = await _context.Projects.FindAsync(new object[] { id});
+
+            var entityFound = dbEntity is not null;
+            
+            if (entityFound)
+            {
+                _context.Projects.Remove(dbEntity);
+
+            }
+
+            return entityFound;
+        }
+
+        public override Task<IEnumerable<CoreProject>> Get(int page, int size)
+        {
+            var projects = _context.Projects
+               .AsNoTracking()
+               .Include(p => p.Tasks)
+               .Skip((page - 1) * size)
+               .Take(size)
+               .AsEnumerable();
+
+            return Task.FromResult(_mapper.Map<IEnumerable<CoreProject>>(projects));
+        }
+
+        public override async Task<CoreProject> Get(Guid id)
+        {
+            var dbEntity = await _context.Projects
+                .AsNoTracking()
+                .Include(p => p.Tasks)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            return MapToCore(dbEntity);
+        }
+
+        public override async Task<Guid> Update(CoreProject entity)
+        {
+            var dbEntity = await _context.Projects.FindAsync(new object[]{ entity.Id});
+
+            dbEntity = MapToEntity<EntityProject>(entity);
+
+            return dbEntity.Id;
+        }
+    }
+}
--- Server/TaskManager.WebAPI/TaskManager.DataAccess/Repositories/RepositoryBase.cs
+++ Server/TaskManager.WebAPI/TaskManager.DataAccess/Repositories/RepositoryBase.cs
@@ -6,13 +6,13 @@ namespace TaskManager.DataAccess.Repositories
 {
     public abstract class RepositoryBase<T, C> : IRepository<T> where C : ProjectsContext
     {
-        protected C _usersContext;
+        protected C _context;
 
         protected readonly IMapper _mapper;
 
-        public RepositoryBase(C usersContext, IMapper mapper)
+        public RepositoryBase(C context, IMapper mapper)
         {
-            _usersContext = usersContext;
+            _context = context;
 
             _mapper = mapper;
         }
@@ -29,15 +29,15 @@ namespace TaskManager.DataAccess.Repositories
 
         protected virtual async Task Save<T>(T entity, EntityState entityState)
         {
-            _usersContext.Entry(entity).State = entityState;
+            _context.Entry(entity).State = entityState;
 
-            await _usersContext.SaveChangesAsync();
+            await _context.SaveChangesAsync();
         }
 
-        protected D MapToEntity<D>(T invation) =>
-         _mapper.Map<T, D>(invation);
+        protected D MapToEntity<D>(T entity) =>
+         _mapper.Map<T, D>(entity);
 
-        protected T MapToCore<D>(D invation) =>
-            _mapper.Map<D, T>(invation);
+        protected T MapToCore<D>(D entity) =>
+            _mapper.Map<D, T>(entity);
     }
 }
